5758e5672e51ad0db3e61e025f69a31d685cdfab
Cogu <wfrwellington13@gmail.com>
2026-06-19 22:05:27 -0300

    .

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,68 +1,56 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
+const mysql = require('mysql2');
 
-const {
-  readReceptionData,
-  saveReceptionData,
-  readGraphData,
-  saveGraphData
-} = require("./modules/storage");
+let mysqlConnection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "131213",
+    database: "site_db"
+});
 
-const {
-  generateGraphData
-} = require("./modules/statistics");
-
-
+app.use(cors());
 app.use(express.json());
-app.use(express.static("src/frontend"));
 
+app.post("/register-user", (req, res) => {
+    let data = req.body
+    if (!data.email || !data.pass) return res.status(500).json({ error: 'Email e Senha inválidos' });
 
-
-app.post("/salvar", (req, res) => {
-  try {
-    const newData = req.body;
-
-    const receptionData =
-      readReceptionData();
-
-    receptionData.push(newData);
-
-    saveReceptionData(receptionData);
-
-
-    const graphData =
-      generateGraphData(
-        receptionData
-      );
-
-    saveGraphData(graphData);
-
-    res.json({
-      message: "Salvo com sucesso"
+    let sql = `INSERT INTO users (email, pass) VALUES ('${data.email}', '${data.pass}')`;
+    mysqlConnection.query(sql, function (err, result) {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: err });
+        } else {
+            console.log("New user registred");
+            res.json({ data: result })
+        }
     });
-
-
-
-  } catch (error) {
-
-    console.error(error);
-
-    res.status(500).json({
-      error: error.message
-    });
-  }
-
 });
 
-app.get("/grafico", (req, res) => {
+app.post("/login-user", (req, res) => {
+    let data = req.body
+    if (!data.email || !data.pass) return res.status(500).json({ error: 'Email e Senha inválidos' });
 
-  const graphData =
-    readGraphData();
-
-  res.json(graphData);
+    let sql = `SELECT * from users WHERE email = '${data.email}' and pass = '${data.pass}'`;
+    mysqlConnection.query(sql, function (err, result) {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: err });
+        } else {
+            res.json({ data: result })
+        }
+    });
 });
 
-app.listen(3000, () => {
-  console.log("Servidor rodando em http://localhost:3000");
+
+mysqlConnection.connect(function(err) {
+    if (err) throw err;
+    console.log("MYSQL Connected!");
+});
+
+app.listen(3001, () => {
+    console.log("Server on http://localhost:3001");
 });
---
+++ FrontEnd/JS/login/index.js
@@ -0,0 +1,89 @@
+let serverUrl = 'http://localhost:3001'
+let siteUrl = 'http://localhost:3000'
+
+let createAccountPageButton = document.getElementById("createAccountPageButton")
+let loginPageButton = document.getElementById("loginPageButton")
+
+let loginContent = (document.getElementsByClassName("login"))[0]
+let registerContent = (document.getElementsByClassName("register"))[0]
+
+let loginButton = document.getElementById("loginButton")
+let registerButton = document.getElementById("registerButton")
+
+let loginAlert = (document.getElementsByClassName("alert login"))[0].getElementsByTagName('label')[0]
+let registerAlert = (document.getElementsByClassName("alert register"))[0].getElementsByTagName('label')[0]
+
+
+
+createAccountPageButton.addEventListener(`click`, (event) => {
+    loginContent.style.display = 'none'
+    registerContent.style.display = 'block'
+    registerAlert.style.display = 'none'
+})
+
+loginPageButton.addEventListener(`click`, (event) => {
+    loginContent.style.display = 'block'
+    registerContent.style.display = 'none'
+    loginAlert.style.display = 'none'
+})
+
+
+
+let newAlert = (element, text) => {
+    element.style.display = 'block'
+    element.innerText = text
+}
+
+loginButton.addEventListener('click', async(event) => {
+    let loginEmailInput = document.getElementById("loginEmailInput")
+    let loginPasswordInput = document.getElementById("loginPasswordInput")
+
+    if (!loginEmailInput.value || !(loginEmailInput.value.includes('@') && loginEmailInput.value.includes('.'))) return newAlert(loginAlert, 'Você deve adicionar um email válido!')
+    if (!loginPasswordInput.value) return newAlert(loginAlert, 'Você deve adicionar uma senha!')
+    if (loginPasswordInput.value.length < 8 || loginPasswordInput.value.length > 32) return newAlert(loginAlert, 'A senha deve conter entre 8 a 32 digitos!')
+
+    const response = await fetch(serverUrl+'/login-user', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: loginEmailInput.value,
+            pass: loginPasswordInput.value
+        })
+    }).then((res) => res.json());
+
+    if (response.error) return newAlert(loginAlert, response.error)
+
+    if (!response.data || !response.data[0]) return newAlert(loginAlert, 'Email ou senhas incorretos!')
+    else {
+        window.location.href = siteUrl+'/HTML/dashboard.html'
+        console.log(response.data)
+    }
+})
+
+registerButton.addEventListener('click', async (event) => {
+    let registerEmailInput = document.getElementById("registerEmailInput")
+    let registerPasswordInput = document.getElementById("registerPasswordInput")
+    let registerRepeatPasswordInput = document.getElementById("registerRepeatPasswordInput")
+
+    if (!registerEmailInput.value || !(registerEmailInput.value.includes('@') && registerEmailInput.value.includes('.'))) return newAlert(registerAlert, 'Você deve adicionar um email válido!')
+    if (!registerPasswordInput.value) return newAlert(registerAlert, 'Você deve adicionar uma senha!')
+    if (registerPasswordInput.value.length < 8 || registerPasswordInput.value.length > 32) return newAlert(registerAlert, 'A senha deve conter entre 8 a 32 digitos!')
+    if (registerPasswordInput.value != registerRepeatPasswordInput.value) return newAlert(registerAlert, 'As senhas não conferem!')
+
+    const response = await fetch(serverUrl+'/register-user', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: registerEmailInput.value,
+            pass: registerPasswordInput.value
+        })
+    }).then((res) => res.json());
+
+    if (response.error) return newAlert(registerAlert, response.error)
+
+    window.location.href = siteUrl+'/HTML/deshboard.html'
+})
